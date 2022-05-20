c6cb860b2eb61c68197407b2871d9ed8fc5383e2
Poilon <poilon@gmail.com>
2022-05-20 17:44:37 +0200

    future

--- lib/binance/spot/futures.rb
+++ lib/binance/spot/futures.rb
@@ -13,7 +13,7 @@ module Binance
       # @option kwargs [String] :symbol
       # @option kwargs [Integer] :recvWindow The value cannot be greater than 60000
       # @see https://binance-docs.github.io/apidocs/futures/en/#symbol-price-ticker
-      def futures_symbol_price_ticker(**kwargs)
+      def future_symbol_price_ticker(**kwargs)
         @session.base_url = "https://fapi.binance.com"
         result = @session.sign_request(:get, '/fapi/v1/ticker/price', params: kwargs)
         @session.base_url = "https://api.binance.com"
@@ -27,7 +27,7 @@ module Binance
       # @param symbol [String]
       # @option kwargs [Integer] :recvWindow The value cannot be greater than 60000
       # @see https://binance-docs.github.io/apidocs/futures/en/#account-trade-list-user_data
-      def futures_trades(symbol:, **kwargs)
+      def future_trades(symbol:, **kwargs)
         Binance::Utils::Validation.require_param('symbol', symbol)
 
         @session.base_url = "https://fapi.binance.com"
